caea70cb8da15950deb16c234d7d6113d28b5d32
arktorius <lunient@gmail.com>
2020-12-16 14:36:08 +0100

    replace files

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {Router} from 'express'
+
+const router = Router();
+
+
+router.get('/tasks', (req,res)=>{
+    res.json('adsasdasdasd')
+})
+
+
+export default router
