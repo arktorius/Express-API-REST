adf7b2e053104c147c895b81e04ab6736f577f74
arktorius <lunient@gmail.com>
2020-12-18 19:21:44 +0100

    First model is done

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ import router from './routes/tasks.routes'
 
 const app = express();
 
-
+//PORT
 app.set('port', process.env.PORT || 3500)
 
-app.get('/',(req,res)=>{
-    res.json('hola mundo2')
-})
+//JSON
+app.use(express.json());
+
+//ROUTES
 app.use('/api',router)
 
 export default app
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,11 +1,27 @@
 import express from "express";
 import {Router} from 'express'
+import model from '../models/UserModel'
 
 const router = Router();
 
 
-router.get('/tasks', (req,res)=>{
+router.get('/User', (req,res)=>{
     res.json('adsasdasdasd')
+})
+
+router.post('/User',async (req, res)=>{
+    console.log(req.body);
+    const newUser = new model({
+        UserName: req.body.UserName,
+        UserFullName: req.body.UserFullName,
+        UserPassword:req.body.UserPassword,
+        UserIsAviable: req.body.UserIsAviable
+         
+    });
+    await newUser.save();
+    console.log(newUser);
+    res.json('ok');
+
 })
 
 
